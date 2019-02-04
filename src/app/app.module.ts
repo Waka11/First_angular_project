a4727b0fdfa308f1b33b22f138adc90cdc04b723
Andy Pavliuk <andypavliuk11@gmail.com>
2019-02-04 19:42:30 +0200

    Angular test
    Testing Angular abilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,10 @@ import { AppComponent } from "./app.component";
 import { HeadComponent } from "./head/head.component";
 import { MainComponent } from "./main/main.component";
 import { ListComponent } from "./main/list/list.component";
-import { FootComponent } from "./foot/foot.component";
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeadComponent, MainComponent, ListComponent, FootComponent, FooterComponent],
+  declarations: [AppComponent, HeadComponent, MainComponent, ListComponent, FooterComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,18 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent implements OnInit {
+  name: string = "FOOTER";
+  online: number = 48;
+  siteName: string = "Angular";
 
-  name:string = 'FOOTER';
-  online:number = 48;
+  button = false;
+  className = "btn btn-dark";
 
-  constructor() { }
+  rocketState = "READY";
+  rocketName = null;
 
-  ngOnInit() {
+  start(){
+    this.rocketState = "LAUNCHED";
+    this.button = false;
+    this.className = "btn btn-dark";
+    setTimeout(() => {
+      this.button = true;
+      this.className = "btn btn-primary";
+      this.rocketState = "READY";
+    }, 5000);
   }
 
+  restart(event:Event){
+    this.rocketName = (<HTMLInputElement>event.target).value;
+  }
+
+  getSiteName() {
+    return this.siteName;
+  }
+
+  constructor() {
+    setTimeout(()=>{
+      this.button = true;
+      this.className = "btn btn-primary";
+    },3000);
+  }
+
+  ngOnInit() {}
 }
