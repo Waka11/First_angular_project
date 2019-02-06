f24f2867a7fe73981feae74e5aae480227599985
Andy Pavliuk <andypavliuk11@gmail.com>
2019-02-06 15:58:19 +0200

    Testing Angular

--- src/app/main/list/list.component.ts
+++ src/app/main/list/list.component.ts
@@ -5,4 +5,30 @@ import { Component } from "@angular/core";
   templateUrl: "./list.component.html",
   styleUrls: ["./list.component.css"]
 })
-export class ListComponent {}
+export class ListComponent {
+  name = null;
+  surname = null;
+  age = null;
+
+  savedName = null;
+  savedSurname = null;
+  savedAge = null;
+
+  takeName(event: Event) {
+    this.name = (<HTMLInputElement>event.target).value;
+  }
+
+  takeSurname(event: Event) {
+    this.surname = (<HTMLInputElement>event.target).value;
+  }
+
+  takeAge(event: Event) {
+    this.age = (<HTMLInputElement>event.target).value;
+  }
+
+  saveInfo() {
+    this.savedName = this.name;
+    this.savedSurname = this.surname;
+    this.savedAge = this.age;
+  }
+}
